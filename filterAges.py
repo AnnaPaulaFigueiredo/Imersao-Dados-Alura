d35a6ee59c5748f52758b3190e33b9d021164d95
Anna Paula Figueiredo <50758471+AnnaPaulaFigueiredo@users.noreply.github.com>
2020-10-21 08:26:08 -0300

    Add files via upload
    Add do file para realizar o carregamento do arquivo MICRODADOS original, e a seleção completa dos alunos menores de idade.

---
+++ filterAges.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+i = 0
+for chunk in pd.read_csv("MICRODADOS_ENEM_2019.csv", delimiter=";",encoding="ISO-8859-1",low_memory=False,chunksize=50000):
+    
+    data = chunk.query("NU_IDADE < 18")
+
+    if i == 0:
+
+        dfAges = pd.DataFrame(data)
+
+    if len(data) > 0:
+
+        
+        dfAges.append(data, ignore_index=True)
+
+    i = i + 1 
+    
+   
+
+print(dfAges.head())
+dfAges.to_csv("dataMinorAge.csv")
+
